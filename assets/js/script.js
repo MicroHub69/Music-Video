148f18ba4ce7bb147e8659b847119d6db5e274b1
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-10-06 13:50:55 +0100

    Other

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
 ///SEARCH BAR
+
+
+
+
+
+$(documennt).ready(function(){
+
+
+    $("#search-form").submit(function(e){
+        e.preventDefault()
+    })
+
+
+
+    function search(){
+        $("#results").html("")
+        $("#buttons").html("")
+    
+        ///GET FORM INPUTS
+        q = $("#query").val();
+    
+        ////GET RUN REQUEST ON API
+        $.get(
+            "https://www.googleapis.com/youtube/v3/search",{
+                part: 'snippet, id',
+                q: q,
+                type: 'video',
+                key: "AIzaSyCWqsbZfTuSO4xxIhlOw0a8RM-s7Da6YIA"},
+                function(data){
+                    var nextPageToken = data.nextPageToken;
+                    var prevPageToken = data.prevPageToken;
+    
+                    console.log(data)
+    
+                    
+                }
+        )
+    }
+})
+
