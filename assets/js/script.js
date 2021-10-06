bf08b818913d31296049262ea79b91dbfaa0cd5c
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2021-10-06 14:35:50 +0100

    Hello

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,65 @@
 
 
 
-$(documennt).ready(function(){
+$(document).ready(function(){
 
 
     $("#search-form").submit(function(e){
-        e.preventDefault()
+        e.preventDefault();
     })
-
-
-
-    function search(){
-        $("#results").html("")
-        $("#buttons").html("")
-    
-        ///GET FORM INPUTS
-        q = $("#query").val();
-    
-        ////GET RUN REQUEST ON API
-        $.get(
-            "https://www.googleapis.com/youtube/v3/search",{
-                part: 'snippet, id',
-                q: q,
-                type: 'video',
-                key: "AIzaSyCWqsbZfTuSO4xxIhlOw0a8RM-s7Da6YIA"},
-                function(data){
-                    var nextPageToken = data.nextPageToken;
-                    var prevPageToken = data.prevPageToken;
-    
-                    console.log(data)
-    
-                    
-                }
-        )
-    }
 })
+
+
+
+function search(){
+    $("#results").html("")
+    $("#buttons").html("")
+
+    ///GET FORM INPUTS
+    q = $("#query").val();
+
+    ////GET RUN REQUEST ON API
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search",{
+            part: 'snippet, id',
+            q: q,
+            type: 'video',
+            key: "AIzaSyCWqsbZfTuSO4xxIhlOw0a8RM-s7Da6YIA"},
+            function(data){
+                var nextPageToken = data.nextPageToken;
+                var prevPageToken = data.prevPageToken;
+                //Log Data
+                console.log(data)
+
+                $(data.items, function(i, item){
+                    ///GET OUTPUT
+                    var output = getOutput(item);
+
+                    //Display Results
+                    $("#results").append(output)
+                })
+            }
+    )
+}
+
+////Build OUTPUT
+function getOutput(item){
+    var videoId = item.id.videoId;
+    var title = item.snippet.title;
+    var description = item.snippet.description;
+    var thumb = item.snippet.thumbnails.high.url;
+    var channelTitle = item.snippet.channelTitle;
+    var videoDate = item.snippet.publishedAt;
+
+
+    ///Build Output
+    var output = '<li>' +
+    '<div class"list-left">' + 
+    '<img src"'+thumb+'">' +
+    '</div>' +
+    
+    '<div class"list-right">' +
+    '<h3>'+title+'</h3>' 
+    '<h3>'+title+'</h3>'
+}
 
